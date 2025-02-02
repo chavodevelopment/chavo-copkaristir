9d57a8e9cbd12835abddea2f1c022e5fa7997115
chavodevelopment <ysgizax@gmail.com>
2025-02-02 20:42:44 +0300

    Add files via upload

---
+++ chavo-copkaristir/fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'chavo'
+description 'chavo-copkaristir'
+
+--DİSCORD: chavodevelopment
+
+
+client_script 'client/client.lua'
+server_script 'server/server.lua'
+shared_script 'config.lua'
